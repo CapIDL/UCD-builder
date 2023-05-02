42350bf27531ba88d7de84a36b4f0de5be94b656
Jonathan S Shapiro <jonathan.s.shapiro@gmail.com>
2023-05-02 00:55:25 -0700

    Emit correct hex constants

--- property/BinaryProperty.go
+++ property/BinaryProperty.go
@@ -135,7 +135,7 @@ func (bp *BinaryProperty) PrintTo(out io.Writer) {
 	fmt.Fprintf(out, "var %s = &unicode.RangeTable{\n", bp.Name)
 	fmt.Fprintf(out, "  R16: []unicode.Range16{ /* %d */", len(rt.R16))
 	for _, r := range rt.R16 {
-		fmt.Fprintf(out, "\n    unicode.Range16{Lo: %04X, Hi: %04X, Stride: %d },",
+		fmt.Fprintf(out, "\n    unicode.Range16{Lo: 0x%04X, Hi: 0x%04X, Stride: %d },",
 			r.Lo, r.Hi, r.Stride)
 	}
 	if len(rt.R16) > 0 {
@@ -144,7 +144,7 @@ func (bp *BinaryProperty) PrintTo(out io.Writer) {
 	fmt.Fprintf(out, "},\n")
 	fmt.Fprintf(out, "  R32: []unicode.Range32{ /* %d */", len(rt.R32))
 	for _, r := range rt.R32 {
-		fmt.Fprintf(out, "\n    unicode.Range32{Lo: %04X, Hi: %04X, Stride: %d },",
+		fmt.Fprintf(out, "\n    unicode.Range32{Lo: 0x%06X, Hi: 0x%06X, Stride: %d },",
 			r.Lo, r.Hi, r.Stride)
 	}
 	if len(rt.R32) > 0 {
